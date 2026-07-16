dfab05ff2dedb8bfab55358dd6b4ee007f4df568
Abdullah <mhabdullah000@gmail.com>
2026-07-16 11:37:51 +0500

    OOP

---
+++ 10_oop/01_simple_class.py
@@ -0,0 +1,12 @@
+
+class Chai:
+    pass 
+class ChaiTime:
+    pass
+
+
+print(type(Chai))
+lemon_chai=Chai()
+print(type(lemon_chai))
+print(type(lemon_chai) is Chai)
+print(type(lemon_chai) is ChaiTime())
